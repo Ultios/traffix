b3de950abec4d064cd962ffefd38b12f390923e6
Aulia Rahman Bachtiar <rahmancs02@gmail.com>
2023-02-26 16:27:59 +0700

    Makes init, api, and version for publishing

---
+++ StraPy/lpr.py
@@ -0,0 +1,16 @@
+def update_travel_time_lpr(G, alpha=0.15, beta=4):
+    df = nx.to_pandas_edgelist(G, edge_key='ekey').set_index(["source","target","ekey"])
+    df['travel_time'] = df.apply(lambda x: lpr(x, alpha, beta), axis=1).values
+    nx.set_edge_attributes(G, df['travel_time'], name='travel_time')
+    return G
+
+#This is a link performance function derived from Indonesia's Highway Manual (MKJI 1997).
+def lpr(row, alpha=0.15,beta=4):
+    if np.isnan(row['flow']):
+        return row['travel_time']
+    else:
+        try:
+            return row['initial_travel_time']*(1+alpha*(row['flow']/row['capacity'])**beta)
+        except ValueError:
+            return row['travel_time']
+
