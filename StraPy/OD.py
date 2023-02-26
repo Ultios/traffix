249524311955ed9e3c312c58c33ac512c7c92fa7
Aulia Rahman Bachtiar <rahmancs02@gmail.com>
2023-02-26 16:09:34 +0700

    Updating all python files of StraPy with the latest changes

--- StraPy/OD.py
+++ StraPy/OD.py
@@ -5,7 +5,10 @@ def OD_nodes_list(nodes_OD):
         for index, dests in nodes_OD.items():
             yield (origin,dests)
 
-def OD_shortest_path(G, nodes_OD):
+def OD_shortest_path(G, nodes_OD, flow_list, flow_type='flow'):
+    nx.set_edge_attributes(G, 0, flow_type)
     nodes = pd.DataFrame(list(OD_nodes_list(nodes_OD)))
     routes = ox.shortest_path(G, nodes.iloc[:,0], nodes.iloc[:,1], weight="travel_time", cpus=None)
-    return routes
+    for i in range(len(routes)):
+        link_flow(G,routes, i, flow_list[i], flow_type)
+    return G
--- StraPy/ue.py
+++ StraPy/ue.py
@@ -1,46 +1,64 @@
 
 
+def line_search(row, a, alpha, beta):
+    t=row['initial_travel_time']
+    x=row['flow']
+    y=row['auxflow']
+    c=row['capacity']
+    return -t*(x-y)*(alpha*(((x+a*(y-x))/c)**beta)+1)
 
-
-#This is a link performance function derived from Indonesia'Highway Manual (MKJI 1997).
-def lpr_idn(kapasitas,arus,freeflow=57):
-    tf=1/freeflow*3600/1000
-    return tf*(1+0.79*(arus/kapasitas)**3.24)
-
-def bisection(qn,yn,xl=0,xr=1, delta=0.0001):
-    n=0
-    dn=yn-qn
-    function=lambda x:qn-x*(dn)
-    while abs((xr-xl)/2)> delta:
-        n+=1
-        x1=(xr+xl-delta)/2
-        x2=(xr+xl+delta)/2
-        f1=function(x1)
-        f2=function(x2)
-        if f1>f2:
-            xr=x2
-        elif f1<f2:
-            xl=x1
-        elif f1==f2:
-            xr=x2
-            xl=x1
+def bisection(G, xl=0, xr=1, delta=0.0002, alpha=0.15, beta=4):
+    n = 0
+    df = nx.to_pandas_edgelist(G)
+    condition = True
+    while condition:
+        n += 1
+        x = (xl+xr)/2
+        if df.apply(lambda row: line_search(row, x, alpha=alpha, beta=beta), axis = 1).sum() <= 0:
+            xl = x
         else:
-            raise ValueError("Ada Kesalahan")
-    print(f1)
-    print(f2)
-    print(n)
-    return round((xr+xl)/2,3)
+            xr = x
+        condition = abs(xr-xl) > 2*delta
+        # print('|xl-xr| =', abs(xr-xl),' xl =', xl, ' xr=',xr)
+    #print(f"number of iteration in bisection method is {n} with alpha = {(xr+xl)/2}")
+    return (xr+xl)/2
 
-#Temporary list for routes.
-def listtemp(routes, flow):
-    saved = []
+def update(row, alpha):
+    return row['flow'] + alpha * (row['auxflow'] - row['flow'])
+
+def update_mainflow(G, alpha):
+    df = nx.to_pandas_edgelist(G, edge_key='ekey').set_index(["source","target","ekey"])
+    df['flow'] = df.apply(lambda row: update(row, alpha), axis=1).values
+    nx.set_edge_attributes(G, df['flow'], name='flow')
+    return G  
+
+def CCA(G, nodes_OD, flow, number_of_iteration=10000, alpha=0.15, beta=4):
+    #Initialize initial time_travel column based on travel time column 
+    #from ox.add_edge_travel_times
+    G = add_edge_initial_travel_time(G)
     
-    for x, y in zip(routes,np.nditer(flow)):
-        if isinstance(x, list):
-            for number in x:
-                saved.append([number,int(y)])
-        elif x == None:
-            continue
-        else:
-            saved.append([x,int(y)])
-    return saved
+    # Initialize flow from OD Matrices, do shortest path
+    # and Assign flow to all links.
+    G = OD_shortest_path(G, nodes, flow, flow_type='flow')
+    
+    condition = True
+    n = 0
+    while condition == True:
+
+        #Update travel time
+        G = update_travel_time_lpr(G, alpha, beta)
+
+        # Initialize flow from OD Matrices, do shortest path
+        # and calculate auxiliary flow.
+        G = OD_shortest_path(G, nodes, flow, flow_type='auxflow')
+
+        # line search using bisection method
+        alpha = bisection(G, alpha=alpha, beta=beta)
+        if n > number_of_iteration:
+            condition = False
+        alpha_old = alpha
+        #updating mainflow based on optimal move size and auxflow
+        G = update_mainflow(G, alpha)
+        n+=1
+        print(n)
+    return G
